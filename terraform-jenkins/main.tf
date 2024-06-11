a4cdb265ed38c3e13902f456aa52164834680fd0
LasyaSriG <163024756+LasyaSriG@users.noreply.github.com>
2024-06-11 11:24:54 +0530

    Update main.tf

--- terraform-jenkins/main.tf
+++ terraform-jenkins/main.tf
@@ -2,6 +2,13 @@ provider "google" {
   project     = "sumanth-97"
 }
 
+terraform {
+  backend "gcs" {
+    bucket  = "sumanth-state-backup-bucket"
+    prefix  = "terraform/state"
+  }
+}
+
 module "jenkins-server" {
 source = "/root/get-ubuntudesktop-iac/terraform-jenkins/modules/jenkins-server"
 }
