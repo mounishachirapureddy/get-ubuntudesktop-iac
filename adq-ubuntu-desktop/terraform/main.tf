7730db9f2f7151668a10d298cc6f57c1e2f13aa7
summu97 <135579543+summu97@users.noreply.github.com>
2024-06-28 09:30:10 +0530

    Update main.tf

--- adq-ubuntu-desktop/terraform/main.tf
+++ adq-ubuntu-desktop/terraform/main.tf
@@ -2,8 +2,8 @@ provider "google" {
   project     = var.project_id
 }
 
-module "adq-ubuntu-desktop" {
-source = "/root/PROJECT-01/adq-ubuntu-desktop/terraform/modules/ubuntu-desktop"
+module "ubuntu-desktop" {
+source = "/var/lib/jenkins/workspace/desktop/adq-ubuntu-desktop/terraform/modules/ubuntu-desktop"
 
 desktop_server_name 			= var.desktop_server_name
 get_desktop_machine_type		= var.get_desktop_machine_type
