a64a15bb04b3fdb5dbcd239e7a46b994a446c058
summu97 <135579543+summu97@users.noreply.github.com>
2024-07-03 09:03:51 +0530

    Update main.tf

--- adq-jenkins-box/main.tf
+++ adq-jenkins-box/main.tf
@@ -1,5 +1,6 @@
 provider "google" {
   project     = var.project_id
+  region      = var.project_region
 }
 
 terraform {
@@ -18,9 +19,9 @@ get_jenkins_zone                = var.get_jenkins_zone
 get_jenkins_image               = var.get_jenkins_image
 get_jenkins_boot_size           = var.get_jenkins_boot_size
 get_jenkins_network_tag         = var.get_jenkins_network_tag
-network				= var.network
-subnetwork			= var.subnetwork
-svc_email   			= var.svc_email
-jenkins_box_firewall_name	= var.jenkins_box_firewall_name
-
+network				                  = var.network
+subnetwork			                = var.subnetwork
+svc_email   			              = var.svc_email
+jenkins_box_firewall_name	      = var.jenkins_box_firewall_name
+jenkins_box_static_ip_name      = var.jenkins_box_static_ip_name
 }
